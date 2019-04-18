6d485f7bc9ce6b26f2d61707036295ad5152a582
trycatchhorn <trycatchhorn@gmail.com>
2019-04-18 15:04:36 +0200

    Added new package

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from package_one.module_a.class_a import ClassA
 from package_one.module_b.class_b import ClassB
+from package_one.module_common.class_common import ClassCommon
+from package_two.module_c.file_c import func_c
 
 if __name__ == "__main__":
     object_a = ClassA()
@@ -8,4 +10,8 @@ if __name__ == "__main__":
     object_b = ClassB()
     object_b.method_b()
     object_b.method_reference_module_a()
+
+    object_common = ClassCommon()
+    object_common.method_common()
+    func_c()
 
--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-python3 main.py
+echo "Running python"
+python main.py
+
+# echo "Running python3"
+# python3 main.py
 
 find . -name "*.py~" -type f -delete
 find . -name "*.pyc" -type f -delete
+find . -name "*.sh~" -type f -delete
 find . -name "__pycache__" -type d -delete
+
 
